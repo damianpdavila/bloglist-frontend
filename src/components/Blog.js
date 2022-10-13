08465e2aeb7606ad87f770ac723984fbb43ec1da
Damian Davila <damian@moventisusa.com>
2022-10-13 16:58:03 -0400

    Exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 10,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    paddingBottom: 10,
+    borderColor: '#aaa'
+  }
+    const [view, setView] = useState(false);
+
+    const toggleView = (viewHide) => { setView(!view); };
+
+    if (!view) {
+        return (
+            <div style={blogStyle}>
+                {blog.title} {blog.author} <button onClick={() => setView(!view)}>View</button>
+            </div>
+        )
+    } else {
+        return (
+            <div style={blogStyle}>
+                Title: {blog.title} <button onClick={() => setView(!view)}>Hide</button>
+                <br/>Author: {blog.author}
+                <br/>URL: {blog.url}
+                <br/>Likes: {blog.likes} <button>Like</button>
+                <br/>Submitted by: {blog.user}
+            </div>
+        );
+    }
+};
+
+export default Blog;
