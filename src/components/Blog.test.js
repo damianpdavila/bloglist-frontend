faee321cf6005225579b57463f79f5244a92c759
Damian Davila <damian@moventisusa.com>
2022-11-03 19:06:09 -0400

    Exercise 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog';
 
 test('renders content', () => {
@@ -23,4 +24,28 @@ test('renders content', () => {
     expect(div).toHaveTextContent('Author Test Render Content');
     expect(div).not.toHaveTextContent('https://testrender.com');
     expect(div).not.toHaveTextContent('Likes:');
+});
+
+test('clicking view button shows blog details', async () => {
+    const blog = {
+        title: 'Title Test Render Content',
+        author: 'Author Test Render Content',
+        url: 'https://testrender.com',
+        likes: 0,
+        user: {username: 'damian'}
+    };
+
+    const mockHandler = jest.fn()
+
+    const {container} = render(<Blog blog={blog} username={'damian'} addLike={mockHandler} deleteBlog={mockHandler} />);
+    const user = userEvent.setup()
+    const button = screen.getByText('View')
+    await user.click(button)
+
+    const div = container.querySelector('.blog')
+
+    expect(div).toHaveTextContent('Title Test Render Content');
+    expect(div).toHaveTextContent('Author Test Render Content');
+    expect(div).toHaveTextContent('https://testrender.com');
+    expect(div).toHaveTextContent('Likes:');
 });
